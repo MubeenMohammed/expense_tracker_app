6f31bf6c7f01fd8442606885fdd7e593907ae6db
MubeenMohammed <mubeen.mohammed@mail.mcgill.ca>
2024-05-28 10:10:34 -0400

    creating the expense widgets

---
+++ lib/widgets/expenses.dart
@@ -0,0 +1,43 @@
+import 'package:expense_tracker_app/widgets/expenses_list/expense_list.dart';
+import 'package:expense_tracker_app/models/expense.dart';
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() {
+    return _ExpenseState();
+  }
+}
+
+class _ExpenseState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+        title: 'Flutter Course',
+        amount: 19.99,
+        date: DateTime.now(),
+        category: Category.work),
+    Expense(
+        title: 'Cinema',
+        amount: 15.69,
+        date: DateTime.now(),
+        category: Category.leisure),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          const Text("The chart"),
+          Expanded(
+            child: ExpenseList(
+              expenses: _registeredExpenses,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
