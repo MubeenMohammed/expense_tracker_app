3805827800a4f8586af61dca4702ba5f4821108c
MubeenMohammed <mubeen.mohammed@mail.mcgill.ca>
2024-05-29 19:54:33 -0400

    updating interactive theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:expense_tracker_app/widgets/expenses.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 var kColorScheme =
     ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 96, 59, 181));
@@ -10,6 +11,11 @@ var kDarkColorScheme = ColorScheme.fromSeed(
 );
 
 void main() {
+  /*
+   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((fn) {
+         */
   runApp(
     MaterialApp(
       darkTheme: ThemeData.dark().copyWith(
@@ -51,4 +57,5 @@ void main() {
       home: const Expenses(),
     ),
   );
+  /* }); */
 }
